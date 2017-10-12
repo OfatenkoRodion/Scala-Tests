53c459465de66553a01faa952aef4a3e05bd2bfb
Rodion <madnessjust4@gmail.com>
2017-10-12 22:29:06 +0300

    arrays task 4-6

--- Test2/src/Arrays2.scala
+++ Test2/src/Arrays2.scala
@@ -19,13 +19,30 @@ object Arrays2 extends  App {
       b(i+1)=temp
     }
   println(b)
-
-  var newB=for (i <- 0 until (lastItemToSort,2)) yield {
-    var temp=b(i)
-    b(i)=b(i+1)
-    b(i+1)=temp
-    temp
+  println(b.size)
+  var temp=0
+  var newB=for (i <- 0 to lastItemToSort) yield {
+   /* if (i%2==0){
+      temp=b(i)
+      b(i+1)
+    } else temp*/
+    b(i)
   }
   println(newB)
 
+  val negative= ArrayBuffer[Int]()
+  val pozitive= ArrayBuffer[Int]()
+  val first= ArrayBuffer[Int](1,1,6,-6,-8,6,-6,63,100,-2)
+
+  for (i <- 0 until first.size){
+    if (first(i)<0){
+      negative+=first(i)
+    } else pozitive+=first(i)
+  }
+  val rez= ArrayBuffer[Int]()
+  rez++=negative
+  rez++=pozitive
+  println(rez)
+  println(rez.sortWith(_ < _))
+  println(rez.sortWith(_ > _))
 }
